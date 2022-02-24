b24260be48cfc827cf10635e401ba433d4b23ced
weitao620 <15811321409@163.com>
2022-02-24 14:55:15 +0800

    教育版

--- src/views/Model/ModelReport.vue
+++ src/views/Model/ModelReport.vue
@@ -51,14 +51,14 @@
                 <img src="../../assets/images/woman.png" alt=""> 女
               </div>
             </li>
-            <li>
+            <li style="diplay:flex">
               <img
                 class="dt_per5"
                 src="../../assets/images/report/person3.png"
                 alt=""
               />
               <span class="dt_blod">学&nbsp;&nbsp;籍&nbsp;号：</span>
-              <span>{{details.passport}}</span>
+              <span style="flex:1;word-break:break-all">{{details.passport}}999999999999999999</span>
             </li>
             <li>
               <img
